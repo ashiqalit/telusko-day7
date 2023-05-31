526bc922dd4d97b1e9d5a875c62b988da96c5480
ashiqalit <ashiqalit@gmail.com>
2023-05-31 09:58:13 +0530

    Short url spring mvc

--- src/main/java/com/shortURL/shortURL/ShortUrlApplication.java
+++ src/main/java/com/shortURL/shortURL/ShortUrlApplication.java
@@ -2,12 +2,27 @@ package com.shortURL.shortURL;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 @SpringBootApplication
 public class ShortUrlApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ShortUrlApplication.class, args);
 	}
+	
+	@Configuration
+    public class CorsConfig implements WebMvcConfigurer {
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://127.0.0.1:5500")
+                    .allowedMethods("POST")
+                    .allowedHeaders("*");
+        }
+    }
 
 }
